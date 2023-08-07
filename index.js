a7e01ab8b1c41d4d053048f5f0f16f8ce72f754f
sarahkgreen <sarahkgreen317@gmail.com>
2023-08-07 10:54:38 -0500

    add todo

--- index.js
+++ index.js
@@ -18,12 +18,28 @@ function App(){
     }        
   ])
 
+  const [value, setValue] = React.useState('');
+  const handleSubmit = e =>{
+      e.preventDefault();
+      if (!value) return;
+      const newTodos = [...todos, {text:value, isCompleted:false}];
+      setTodos(newTodos);
+      setValue('');
+  }
   return(
     <>
       {todos.map((todo, i) => 
         <div className= "todo" key={i}>{todo.text}</div>)}
-    </>
-  );
+      <form onSubmit={handleSumbit}>
+          <input
+            type="text"
+            className="input"
+            value={value}
+            placeholder="Add Todo ..."
+            onChange={e => setValue(e.target.value)}
+            />
+      </form>
+    </>);
 }
 
 ReactDOM.render(
